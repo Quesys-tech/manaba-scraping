5502718797e42d9af521a67a82604f230803baaa
Q <qdevelop@hotmail.com>
2018-12-02 22:28:03 +0900

    slackの通知完成

--- scraping.py
+++ scraping.py
@@ -1,13 +1,15 @@
 import datetime
+import json
+
 import pytz
-from time import sleep
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def GetTable():
@@ -30,15 +32,18 @@ def GetTable():
     driver.find_element_by_name("j_username").send_keys(utid13)
     driver.find_element_by_name("j_password").send_keys(password)
 
-    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.NAME, "_eventId_proceed")))
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.NAME, "_eventId_proceed")))
     driver.find_element_by_name("_eventId_proceed").click()
 
     # 未提出課題のページへ飛ぶ
-    WebDriverWait(driver, 10).until(expected_conditions.url_changes("https://manaba.tsukuba.ac.jp/"))
+    WebDriverWait(driver, 10).until(
+        expected_conditions.url_changes("https://manaba.tsukuba.ac.jp/"))
     driver.get('https://manaba.tsukuba.ac.jp/ct/home_library_query')
 
     # テキスト取得
-    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "stdlist")))
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.CLASS_NAME, "stdlist")))
     soup = BeautifulSoup(driver.page_source, "lxml")
 
     rows = soup.find('table', class_='stdlist').find_all(
@@ -67,17 +72,17 @@ def GetTable():
     return assignments
 
 
-def main():
-    assignments = GetTable()
-
+def FindNearDeadline(assignments):
     criteria_hours = 7*24  # 通知を締切までの残り時間(単位:h)
 
     now = datetime.datetime.utcnow()
     now = now.replace(tzinfo=pytz.utc)
 
+    ret = []
+
     for assignment in assignments:
 
-        if len(assignment['deadline']) == 0: #締切の設定がないものは無視する
+        if len(assignment['deadline']) == 0:  # 締切の設定がないものは無視する
             continue
 
         deadline = datetime.datetime.strptime(
@@ -87,12 +92,40 @@ def main():
 
         timedelta = deadline - now
 
-        if timedelta <= datetime.timedelta(hours=criteria_hours): #基準以内の処理をする
-            print(assignment['title']+'\t' +
-                  str(timedelta)+'\t'+'deadline is near.')
-        else:
-            print(assignment['title']+'\t'+str(timedelta) +
-                  '\t'+'deadline is not near.')
+        if timedelta <= datetime.timedelta(hours=criteria_hours):  # 基準以内の処理をする
+
+            ret.append(
+                {
+                    'title': assignment['title'],
+                    'link': assignment['link'],
+                    'timedelta': timedelta
+                }
+            )
+
+    return ret
+
+
+def push2slack(near_deadlines):
+    webhook_url = 'webhook url'
+
+    message_body = '@channel\n'
+    for assignment in near_deadlines:
+        message_body += '<' + \
+            assignment['link']+'|'+assignment['title'] + \
+            '> 残り時間:'+str(assignment['timedelta']) + '\n'
+
+    requests.post(webhook_url, data=json.dumps({
+        'text': message_body,  # 投稿するテキスト
+        'username': u'manaba 宿題',  # 投稿のユーザー名
+        'icon_emoji': u':ghost:',  # 投稿のプロフィール画像に入れる絵文字
+        'link_names': 1,  # メンションを有効にする
+    }))
+
+
+def main():
+    assignments = GetTable()
+    near_deadlines = FindNearDeadline(assignments)
+    push2slack(near_deadlines)
 
 
 if __name__ == '__main__':
