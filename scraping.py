3759d295d0929a6b1b0b67c39c2661702cb1b6c1
Q M <qdevelop@hotmail.com>
2020-11-13 01:43:40 +0900

    update: 統一認証の仕様変更に対応

--- scraping.py
+++ scraping.py
@@ -12,67 +12,77 @@ def GetTable(username, password):
     with requests.Session() as s:  # セッションを生成
         # ヘッダ偽装
         s.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0','Content-Language': 'ja'}
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0', 'Content-Language': 'ja'}
 
-        r = s.get('https://manaba.tsukuba.ac.jp/')
+        # Javascript非対応のブラウザが飛ばされるページからログインページへ行く
 
-        soup = BeautifulSoup(r.content, "lxml")
-        form = soup.find("form")
+        r = s.get('https://manaba.tsukuba.ac.jp/ct/')  # manabaにアクセスしたらIdpに飛ぶ
+        # Beautifulsoupでページを解析
+        # soup = BeautifulSoup(r.content, 'lxml')
+        # form = soup.find('form')
+        # post_url = 'https://idp.account.tsukuba.ac.jp'+form.get('action')
 
-        post_url = 'https://idp.account.tsukuba.ac.jp'+form.get("action")
+        payload_idp = {
+            'shib_idp_ls_exception.shib_idp_session_ss': '',
+            'shib_idp_ls_success.shib_idp_session_ss': False,
+            'shib_idp_ls_value.shib_idp_session_ss': '',
+            'shib_idp_ls_exception.shib_idp_persistent_ss': '',
+            'shib_idp_ls_success.shib_idp_persistent_ss': False,
+            'shib_idp_ls_value.shib_idp_persistent_ss': '',
+            'shib_idp_ls_supported': '',
+            '_eventId_proceed': ''
+        }
 
-        payload = {
+        # postしたらログインページへ飛ぶ
+        r = s.post(
+            'https://idp.account.tsukuba.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1', data=payload_idp)
+
+        payload_auth = {  # 認証情報
             '_eventId_proceed': '',
             'j_password': password,
             'j_username': username
         }
 
-        # postしたあとcontinueボタンを押すページに飛ばされる
-        r = s.post(post_url, data=payload)
+        # 認証情報を送ったらSAMLのトークンを受け取るページへ飛ぶ
+        r = s.post(
+            'https://idp.account.tsukuba.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s2', data=payload_auth)
 
-        # continueボタンを押すときに送信されるデータを解析
+        # SAMLのトークンを取り出すために読み込む
         soup = BeautifulSoup(r.content, "lxml")
 
-        # continueボタンを押したときに送信される内容
-        payload = {
-            'SAMLResponse': '',
-            'RelayState': '',
+        payload_saml = {
+            'RelayState': soup.find(attrs={'name': 'RelayState'}).get('value'),
+            'SAMLResponse': soup.find(attrs={'name': 'SAMLResponse'}).get('value'),
             'submit': 'Continue'
         }
 
-        payload['RelayState'] = soup.find(
-            attrs={'name': 'RelayState'}).get('value')
-        payload['SAMLResponse'] = soup.find(
-            attrs={'name': 'SAMLResponse'}).get('value')
-
-        post_url = soup.find('form').get('action')
-
-        s.post(post_url, payload)  # continueボタンを押す
+        s.post('https://manaba.tsukuba.ac.jp/Shibboleth.sso/SAML2/POST',
+               payload_saml)  # continueボタンを押す
 
         # manabaの課題ページを取得
         r = s.get('https://manaba.tsukuba.ac.jp/ct/home_library_query')
 
-    # 未提出課題の表を解析
-    soup = BeautifulSoup(r.content, "lxml")
-    rows = soup.find('table', class_='stdlist').find_all(
-        'tr', class_=["row0", "row1"])
-    assignments = list()
+        # 未提出課題の表を解析
+        soup = BeautifulSoup(r.content, 'lxml')
+        rows = soup.find('table', class_='stdlist').find_all(
+            'tr', class_=['row0', 'row1'])
+        assignments = list()
 
-    for row in rows:
+        for row in rows:
 
-        assignment = dict()
+            assignment = dict()
 
-        cells = row.find_all('td')
+            cells = row.find_all('td')
 
-        # 要素のテキストを代入
-        assignment["course"] = cells[2].div.a.text
-        assignment["title"] = cells[1].div.a.text
-        assignment["link"] = "https://manaba.tsukuba.ac.jp/" + \
-            cells[1].div.a.get('href')
-        assignment["deadline"] = cells[4].text
-        assignments.append(assignment)
+            # 要素のテキストを代入
+            assignment['course'] = cells[2].div.a.text
+            assignment['title'] = cells[1].div.a.text
+            assignment['link'] = 'https://manaba.tsukuba.ac.jp/' + \
+                cells[1].div.a.get('href')
+            assignment['deadline'] = cells[4].text
+            assignments.append(assignment)
 
-    return assignments
+        return assignments
 
 
 def FindNearDeadline(assignments, criteria_hours):
